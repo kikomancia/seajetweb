9319642244c826a1f10036c163ddf376c54de686
Kiko <jerichopmancia@gmail.com>
2026-01-22 00:02:05 +0800

    fc--0073 ok

--- admin/php_files/logout.php
+++ admin/php_files/logout.php
@@ -2,6 +2,6 @@
     session_start();
     session_unset();
     session_destroy();
-    header("Location: /../../../../seajetweb/admin/index.php");
+    header("Location: /../../../../admin");
     exit;
 ?>
